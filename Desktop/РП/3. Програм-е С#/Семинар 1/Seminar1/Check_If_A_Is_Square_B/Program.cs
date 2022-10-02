aef7bbd74be4806bb781cdd8e561fd6a46b90585
Renat Prokopiev <prokop4891@gmail.com>
2022-10-02 20:37:41 +0300

    Check if number A is square of number B

---
+++ Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/Check_If_A_Is_Square_B/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number A");
+string value1 = Console.ReadLine();
+int A = int.Parse(value1);
+Console.WriteLine("Enter number B");
+string value2 = Console.ReadLine();
+int B = int.Parse(value2);
+if(A == B*B)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
+
