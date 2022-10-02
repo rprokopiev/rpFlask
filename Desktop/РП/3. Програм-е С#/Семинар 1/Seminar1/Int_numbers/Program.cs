18afdfd8190e9c77a56e4526aa695de756f4652b
Renat Prokopiev <prokop4891@gmail.com>
2022-10-02 22:12:29 +0300

    Int numbers initial

---
+++ Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/Int_numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number");
+string value = Console.ReadLine();
+int x = int.Parse(value);
+int y = -x;
+int count = y;
+while (count <= x)
+{
+    Console.Write(count);
+    count = count + 1;
+    if (count <= x)
+    Console.Write(", ");
+}
