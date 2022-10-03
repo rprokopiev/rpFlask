2bc53422b1a39ec825d9ab7f2057c0bdd33d4b30
Renat Prokopiev <prokop4891@gmail.com>
2022-10-03 20:53:36 +0300

    Find Mac of 2

---
+++ Desktop/РП/3. Програм-е С#/Семинар 1/HW/FindMaxOfTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter Number 1: ");
+string Number1 = Console.ReadLine();
+int N1 = int.Parse(Number1);
+Console.Write("Enter Number 2: ");
+string Number2 = Console.ReadLine();
+int N2 = int.Parse(Number2);
+if(N1>N2)
+{
+    Console.WriteLine(N1);
+}
+else 
+{
+    Console.WriteLine(N2);
+}
