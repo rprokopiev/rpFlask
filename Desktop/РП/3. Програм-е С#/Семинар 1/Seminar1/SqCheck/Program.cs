a476da5ba5509e296eceb99a47feef92505cdda3
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 20:34:11 +0300

    1st

---
+++ Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/SqCheck/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number A ");
+string value1 = Console.ReadLine();
+int A = int.Parse(value1);
+Console.WriteLine("Enter number B ");
+string value2 = Console.ReadLine();
+int B = int.Parse(value2);
+if(A*A==B | B*B==A)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
