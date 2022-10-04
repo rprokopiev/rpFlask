eda47592be651c02e6b9a2e479e6fe0ff3fa49ce
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 20:26:41 +0300

    Div by 4 and 23

---
+++ Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/CheckDiv/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number ");
+string value1 = Console.ReadLine();
+int A = int.Parse(value1);
+if (A % 7 ==0 & A % 23 ==0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+     Console.WriteLine("НЕТ");
+}
