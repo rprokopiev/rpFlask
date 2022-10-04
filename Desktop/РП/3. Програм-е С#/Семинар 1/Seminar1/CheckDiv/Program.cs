e3ea62b8478af93245df73ecd30b5e67e31f21e4
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 20:27:53 +0300

    2nd

--- Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/CheckDiv/Program.cs
+++ Desktop/РП/3. Програм-е С#/Семинар 1/Seminar1/CheckDiv/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Enter number ");
 string value1 = Console.ReadLine();
 int A = int.Parse(value1);
-if (A % 7 ==0 & A % 23 ==0)
+if (A % 7 ==0 && A % 23 ==0)
 {
     Console.WriteLine("ДА");
 }
