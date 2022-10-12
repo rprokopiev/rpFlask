30010482046de076451926aca4221ffb4911166d
Renat Prokopiev <prokop4891@gmail.com>
2022-10-12 21:21:11 +0300

    comments to 3rd

---
+++ Documents/RP/3. С#/Lect/003_FindInMassive/Program.cs
@@ -0,0 +1,18 @@
+﻿// Поиск индекса нужного числа в массиве
+// break - используется для остановки цикла после того, как найдено искомое значение в первый раз
+int [] array = {31,28,73,64,55,64,73,81};
+
+int n = array.Length;
+int find = 64;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
